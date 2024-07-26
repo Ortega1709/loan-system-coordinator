6d3ddeda93db1d4c262dc80966afe6b4f5056b36
ortega <kabweortega@gmail.com>
2024-07-26 15:14:39 +0200

    Initial commit

---
+++ src/main/java/com/ortega/loansystemcoordinator/LoanSystemCoordinatorApplication.java
@@ -0,0 +1,12 @@
+package com.ortega.loansystemcoordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSystemCoordinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanSystemCoordinatorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ortega/loansystemcoordinator/LoanSystemCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortega.loansystemcoordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSystemCoordinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
